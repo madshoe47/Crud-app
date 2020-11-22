0b20e5249a7de76b744906e29ae5a300a1fe22b5
madshoe47 <56107464+madshoe47@users.noreply.github.com>
2020-11-22 17:40:04 +0100

    Navbar

---
+++ vue-firebase-crud/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended grey darken-3">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title"> Mads' CRUD app</span>
+        </router-link>
+        <a class="btn-floating btn-large halfway-fab amber darken-4" href="">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
