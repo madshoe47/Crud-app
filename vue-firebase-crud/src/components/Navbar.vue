73476c9b29ce048c0feecf0f09d31df66c3765a6
madshoe47 <56107464+madshoe47@users.noreply.github.com>
2020-11-22 21:07:59 +0100

    Tilføj opskrifter til firebase

--- vue-firebase-crud/src/components/Navbar.vue
+++ vue-firebase-crud/src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <div class="navbar">
     <nav class="nav-extended grey darken-3">
       <div class="nav-content">
-        <router-link to="">
+        <router-link :to="{ name: 'Index' }">
           <span class="nav-title"> Mads' CRUD app</span>
         </router-link>
         <a class="btn-floating btn-large halfway-fab amber darken-4" href="">
-          <router-link to="">
+          <router-link :to="{ name: 'AddFood' }">
             <i class="material-icons">add</i>
           </router-link>
         </a>
--- vue-firebase-crud/src/router/index.js
+++ vue-firebase-crud/src/router/index.js
@@ -1,24 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Index from '@/components/index'
+import AddFood from '@/components/AddFood'
+
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
+const routes = [{
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Index",
+    component: Index
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/add-food",
+    name: "AddFood",
+    component: AddFood
+  },
 ];
 
 const router = new VueRouter({
